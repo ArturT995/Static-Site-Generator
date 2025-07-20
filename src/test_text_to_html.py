362783ed497baaa19017903840126e4107e9ff9e
ArturT995 <arturtre95@gmail.com>
2025-07-21 00:41:35 +0300

    4. added text_to_html

---
+++ src/test_text_to_html.py
@@ -0,0 +1,28 @@
+import unittest
+
+from htmlnode import LeafNode
+from textnode import TextNode, TextType
+from text_to_html import text_node_to_html_node
+
+class TestTextNode(unittest.TestCase):
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+        
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props, {'src': 'https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp', "alt": "This is an image"})
+
+#def __init__(self, tag, value, props = None): html
+#def __init__(self, text, text_type, url=None): text
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-from enum import Enum
 
 
 class TextType(Enum):
